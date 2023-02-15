7c9cbe5baa248c83e86fb3a9fc74a2d31ecddc35
AbderrazekGraies <112821617+AbderrazekGraies@users.noreply.github.com>
2023-02-15 16:36:55 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+function moleAppear() {
+  var random = Math.floor(Math.random() * jQuery("img").length);
+  jQuery("img").hide().eq(random).show();
+}
+
+function gameover() {
+  clearInterval(SInterval);
+  clearInterval(timeInterval);
+  $("img").hide();
+}
+
+$(document).ready(function () {
+  $("#start").click(function () {
+    let time = 30;
+    let points = 0;
+    $("#points").html(`points ${points}`);
+    $(".timer").html(`${time}s`);
+    $("img").click(function () {
+      points++;
+      $("#points").html(`points ${points}`);
+      $(this).hide();
+    });
+    $("img").hide();
+
+    SInterval = setInterval(function () {
+      moleAppear();
+    }, 750);
+
+    timeInterval = setInterval(function () {
+      time--;
+      $(".timer").html(`${time}s`);
+      if (time === 0) {
+        gameover();
+      }
+    }, 1000);
+
+    $("#stop").click(function () {
+      gameover();
+    });
+  });
+});
